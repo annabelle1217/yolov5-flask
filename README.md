de4c033b76a30444dfa384ed6395ce3c0744b3c1
annabelle1217 <ngxinmin.n@gmail.com>
2022-06-12 14:55:06 +0800

    :sparkles: initial commit

--- README.md
+++ README.md
@@ -1,19 +1,52 @@
-# deploy a yolov5 model using pytorch + flask
+# Flask REST API
 
-## start
+[REST](https://en.wikipedia.org/wiki/Representational_state_transfer) [API](https://en.wikipedia.org/wiki/API)s are
+commonly used to expose Machine Learning (ML)  models to other services. This folder contains an example REST API
+created using Flask to expose the YOLOv5s model from [PyTorch Hub](https://pytorch.org/hub/ultralytics_yolov5/).
 
-```bash
-$ FLASK_ENV=development FLASK_APP=app.py flask run
+## Requirements
+- [Pytorch](https://pytorch.org) >= 1.6. 
+- [Pillow](https://pypi.org/project/Pillow/)
+- [Flask](https://palletsprojects.com/p/flask/)
+
+Install Pytorch with instruction in https://pytorch.org. Then, install the rest with: 
+```shell
+$ pip install Flask Pillow
 ```
-then, visit http://localhost:5000/ in your browser
 
-## environments
+## Run
 
-- pytorch >= 1.6
-- flask
-- pillow
+After Flask installation run:
 
-## reference
-- https://github.com/ultralytics/yolov5
+```shell
+$ python app.py --model yolov5
+```
+To use custom model:
+```shell
+$ python app.py --model best.pt
+```
+
+## Use GUI
+Use GUI to upload and view resut:
+Go to [http://localhost:5000](http://localhost:5000)
+
+## Use Terminal
+Use [curl](https://curl.se/) to perform a request (Recommended: Git Bash):
+
+```shell
+$ curl -X POST -F image=@images/cat.jpg 'http://localhost:5000/v1/object-detection/yolov5s'
+```
+
+The model inference results are returned as a JSON response:
+
+```json
+[{"xmin":384.2033081055,"ymin":35.8358840942,"xmax":823.404296875,"ymax":409.2363891602,"confidence":0.8502380848,"class":0,"name":"cat"}]
+```
+
+## Reference
+- [YOLOv5 Flask REST API](https://github.com/ultralytics/yolov5/tree/master/utils/flask_rest_api)
+- https://github.com/jzhang533/yolov5-flask
 - [Load YOLOv5 from PyTorch Hub ](https://github.com/ultralytics/yolov5/issues/36)
 - https://github.com/avinassh/pytorch-flask-api-heroku
+
+Note: The best.pt provided is self-trained yolov5 model on cats and dogs dataset.
--- app.py
+++ app.py
@@ -1,41 +1,83 @@
 import io
-import os
-import json
+import base64
 from PIL import Image
-
 import torch
 from flask import Flask, jsonify, url_for, render_template, request, redirect
+from pathlib import Path
+import sys
+import argparse
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # current directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
 
 app = Flask(__name__)
+DETECTION_URL = "/v1/object-detection/yolov5s"
 
-RESULT_FOLDER = os.path.join('static')
-app.config['RESULT_FOLDER'] = RESULT_FOLDER
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).autoshape()  # for PIL/cv2/np inputs and NMS
-model.eval()
 
 def get_prediction(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     imgs = [img]  # batched list of images
 
-# Inference
-    results = model(imgs, size=640)  # includes NMS
+    # Inference
+    results = model(imgs, size=640)  # reduce size to get faster inference
     return results
 
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        if 'file' not in request.files:
+def save_result(results):
+    results.render()
+    # results.save()  # save as runs\detect\exp<no>\image0.jpg
+    for im in results.imgs:
+        im_base64 = Image.fromarray(im)
+        im_base64.save("static/result.jpg", format="JPEG")
+
+
+@app.route("/", methods=["GET", "POST"])
+def predict_gui():
+    if request.method == "POST":
+        if "file" not in request.files:
             return redirect(request.url)
-        file = request.files.get('file')
+        file = request.files.get("file")
         if not file:
-            return
+            return redirect(request.url)
 
         img_bytes = file.read()
         results = get_prediction(img_bytes)
-        results.save()  # save as results1.jpg, results2.jpg... etc.
-        os.rename("results0.jpg", "static/results0.jpg")
+        save_result(results)
+        return render_template("result.html")
+    return render_template("index.html")
 
-        full_filename = os.path.join(app.config['RESULT_FOLDER'], 'results0.jpg')
-        return redirect('static/results0.jpg')
-    return render_template('index.html')
+
+@app.route(DETECTION_URL, methods=["POST"])
+def predict():
+    if request.method != "POST":
+        return
+
+    if request.files.get("image"):
+        im_file = request.files["image"]
+        im_bytes = im_file.read()
+        results = get_prediction(im_bytes)
+        save_result(results)
+        return results.pandas().xyxy[0].to_json(orient="records")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
+    parser.add_argument(
+        "--model",
+        default="yolov5",
+        type=str,
+        help="Model option 'yolov5' or a custom model path.",
+    )
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    opt = parser.parse_args()
+
+    if opt.model == "yolov5":
+        model = torch.hub.load(
+            "ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True
+        )
+    else:
+        model = torch.hub.load("ultralytics/yolov5", "custom", path=opt.model)
+    model.eval()
+
+    app.run(host="localhost", port=opt.port)  # debug=True causes Restarting with stat
